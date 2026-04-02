b6f0397c08fe0cb62ddc9f5733f1a12fc709d839
mxw86 <mxw86@example.com>
2026-04-02 14:24:38 +0800

    build

--- clean.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let txt = fs.readFileSync('build_plain2.txt', 'utf8');
-txt = txt.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, '').replace(/\r/g, '');
-fs.writeFileSync('clean.log', txt);
--- clean2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let txt = '';
-try {
-  txt = fs.readFileSync('build_plain3.txt', 'utf8');
-} catch (e) {
-  process.exit(0);
-}
-txt = txt.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, '').replace(/\r/g, '');
-fs.writeFileSync('clean3.log', txt);
